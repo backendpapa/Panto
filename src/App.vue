714fe91a2a38fb3515172f1b19d5cf299426d781
Akeju Paul <akejupaul10@gmail.com>
2022-01-14 13:08:01 +0100

    hero init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
+    <!-- <v-app-bar
       app
       color="primary"
       dark
@@ -35,7 +35,7 @@
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-    </v-app-bar>
+    </v-app-bar> -->
 
     <v-main>
       <router-view/>
@@ -45,6 +45,7 @@
 
 <script>
 import "./css/fonts.css"
+import "./css/size.css"
 
 export default {
   name: 'App',
@@ -54,3 +55,9 @@ export default {
   }),
 };
 </script>
+<style>
+v-application {
+    font-family: 'Gilroy-Light', sans-serif !important;
+    line-height: 1.5;
+}
+</style>
