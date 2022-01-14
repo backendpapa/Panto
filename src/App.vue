824d5e9d1ffde5969a41bd9c264788848e0f9993
Akeju Paul <akejupaul10@gmail.com>
2022-01-14 15:54:58 +0100

    navbar layout

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,18 @@
       fixed
     >
       <div class="d-flex align-center">
-        <p class="font-heavy white--text font-title">Panto</p>
+        <p class="font-heavy white--text font-title mt-5">Panto</p>
       </div>
 
       <v-spacer></v-spacer>
-        <v-btn text  style="text-transform:none;color:white" class="font-weight-bold" plain>Furniture <v-icon>mdi-chevron-down</v-icon></v-btn>
+       <div class="d-none d-sm-flex mt-3">
+         <div class="d-flex align-center">
+          <v-btn text  style="text-transform:none;color:white" class="font-weight-bold" plain>Furniture <v-icon>mdi-chevron-down</v-icon></v-btn>
          <v-btn text  style="text-transform:none;color:white" class="font-weight-bold" plain>Shop</v-btn>
          <v-btn text  style="text-transform:none;color:white" class="font-weight-bold" plain>About Us</v-btn>
          <v-btn text  style="text-transform:none;color:white" class="font-weight-bold" plain>Contact</v-btn>
+       </div>
+       </div>
       <v-spacer></v-spacer>
 
      <v-btn plain icon>
@@ -22,6 +26,7 @@
        <v-icon color="white">mdi-shopping</v-icon>
       <v-avatar size="15" color="orange" style="font-size:108;position:absolute;right:15%;top:-20%" class="white--text font-bold">2</v-avatar>
      </v-btn>
+     <v-app-bar-nav-icon class="white--text d-flex d-sm-none"></v-app-bar-nav-icon>
     </v-app-bar>
 
     <v-main>
